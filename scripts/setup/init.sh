516d94266539b14b7aead5f99f81eccdb6097f5c
TheRedCyclops <76230780+TheRedCyclops@users.noreply.github.com>
2024-09-24 18:24:45 +0200

    fix: add passthough for audio

--- scripts/setup/init.sh
+++ scripts/setup/init.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 chown -R "${MUSICBOX_USER}:${MUSICBOX_USER}" "$MUSICBOX_BIN_DIR" "$MUSICBOX_LOG_DIR" "$MUSICBOX_TMP_DIR" "$MUSICBOX_CFG_DIR" "$MUSICBOX_CACHE_DIR"
+case $MUSICBOX_AUDIO_SERVER in  \
+    wireplumber) apt-get update && apt-get install -y wireplumber;;
+    pulseaudio)  apt-get update && apt-get install -y pulseaudio;
+cat << EOF > /etc/pulse/client.conf
+default-server = unix:$MUSICBOX_PULSEAUDIO_SOCKET
+autospawn = no
+daemon-binary = /bin/true
+enable-shm = false
+
+EOF
+;;
+    alsa) apt-get update && apt-get install -y alsa; addgroup audio; usermod -aG audio "${MUSICBOX_USER}";;
+esac
 su musicbox -c "$MUSICBOX_BIN_DIR"/load
